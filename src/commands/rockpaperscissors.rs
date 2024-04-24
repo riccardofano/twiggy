22428bbb944ed0f258f665099af5b4ac72412d9c
Riccardo Fano <rcr.fano@gmail.com>
2024-04-24 15:29:13 +0200

    Abstract finding opponent

--- src/commands/rockpaperscissors.rs
+++ src/commands/rockpaperscissors.rs
@@ -1,4 +1,4 @@
-use std::{str::FromStr, time::Duration};
+use std::{str::FromStr, sync::Arc};
 
 use crate::{
     common::{
@@ -7,7 +7,7 @@ use crate::{
     Context,
 };
 use anyhow::{bail, Result};
-use poise::serenity_prelude::{ButtonStyle, ReactionType};
+use poise::serenity_prelude::{ButtonStyle, MessageComponentInteraction, ReactionType};
 use serenity::{
     builder::CreateActionRow, collector::ComponentInteractionCollectorBuilder, futures::StreamExt,
 };
@@ -63,78 +63,89 @@ pub async fn rps(ctx: Context<'_>) -> Result<()> {
     let challenger = ctx.author();
     let initial_msg = format!("{challenger} is looking for a rock-paper-scissors opponent!");
     let first_message = send_message_with_row(ctx, initial_msg, create_accept_button()).await?;
+    let message_id = first_message.message().await?.id;
 
-    while let Some(interaction) = first_message
-        .message()
-        .await?
-        .await_component_interaction(ctx)
-        .timeout(Duration::from_secs(600))
-        .await
-    {
-        if interaction.data.custom_id != ACCEPT_BTN {
-            continue;
-        }
-
-        if interaction.user.id == challenger.id {
-            ephemeral_interaction_response(&ctx, interaction, "You cannot fight yourself.").await?;
-            continue;
-        }
-
-        let accepter = interaction.user.clone();
-        let weapon_request = "Choose your weapon!";
-        let row = create_weapons_buttons();
-
-        let (challenger_msg, _) = tokio::try_join!(
-            ctx.send(|f| {
-                f.content(weapon_request)
-                    .ephemeral(true)
-                    .components(|c| c.set_action_row(row.clone()))
-            }),
-            interaction.create_interaction_response(ctx, |r| {
-                r.interaction_response_data(|d| {
-                    d.content(weapon_request)
-                        .ephemeral(true)
-                        .components(|c| c.set_action_row(row.clone()))
-                })
-            }),
-        )?;
-
-        let (challenger_msg, accepter_msg) = tokio::try_join!(
-            challenger_msg.message(),
-            interaction.get_interaction_response(ctx)
-        )?;
-
-        let (challenger_choice, accepter_choice) = tokio::try_join!(
-            get_user_weapon_choice(ctx, challenger_msg.id.0, challenger.id.0),
-            get_user_weapon_choice(ctx, accepter_msg.id.0, accepter.id.0)
-        )?;
-
-        let mut end_msg = format!(
-            "{challenger} picks {}, {accepter} picks {}\n",
-            challenger_choice.to_str(),
-            accepter_choice.to_str()
-        );
-        end_msg.push_str(&match challenger_choice.compare(accepter_choice) {
-            Score::Win => format!("{challenger} wins!"),
-            Score::Loss => format!("{accepter} wins!"),
-            Score::Draw => "It's a draw!".to_owned(),
-        });
-
+    let Some(interaction) = find_opponent(ctx, message_id.0, challenger.id.0).await else {
         first_message
-            .edit(ctx, |m| m.content(end_msg).components(|c| c))
+            .edit(ctx, |m| {
+                m.content(format!("Nobody was brave enough to challenge {challenger}"))
+                    .components(|c| c)
+            })
             .await?;
 
         return Ok(());
-    }
+    };
+
+    let accepter = &interaction.user;
+    let weapon_request = "Choose your weapon!";
+    let row = create_weapons_buttons();
+
+    let (challenger_msg, _) = tokio::try_join!(
+        ctx.send(|f| {
+            f.content(weapon_request)
+                .ephemeral(true)
+                .components(|c| c.set_action_row(row.clone()))
+        }),
+        interaction.create_interaction_response(ctx, |r| {
+            r.interaction_response_data(|d| {
+                d.content(weapon_request)
+                    .ephemeral(true)
+                    .components(|c| c.set_action_row(row.clone()))
+            })
+        }),
+    )?;
+
+    let (challenger_msg, accepter_msg) = tokio::try_join!(
+        challenger_msg.message(),
+        interaction.get_interaction_response(ctx)
+    )?;
+
+    let (challenger_choice, accepter_choice) = tokio::try_join!(
+        get_user_weapon_choice(ctx, challenger_msg.id.0, challenger.id.0),
+        get_user_weapon_choice(ctx, accepter_msg.id.0, accepter.id.0)
+    )?;
+
+    let mut end_msg = format!(
+        "{challenger} picks {}, {accepter} picks {}\n",
+        challenger_choice.to_str(),
+        accepter_choice.to_str()
+    );
+    end_msg.push_str(&match challenger_choice.compare(accepter_choice) {
+        Score::Win => format!("{challenger} wins!"),
+        Score::Loss => format!("{accepter} wins!"),
+        Score::Draw => "It's a draw!".to_owned(),
+    });
 
     first_message
-        .edit(ctx, |m| {
-            m.content(format!("Nobody was brave enough to challenge {challenger}"))
-                .components(|c| c)
-        })
+        .edit(ctx, |m| m.content(end_msg).components(|c| c))
         .await?;
 
     Ok(())
+}
+
+async fn find_opponent(
+    ctx: Context<'_>,
+    message_id: u64,
+    challenger_id: u64,
+) -> Option<Arc<MessageComponentInteraction>> {
+    let mut collector = ComponentInteractionCollectorBuilder::new(ctx)
+        .message_id(message_id)
+        .timeout(std::time::Duration::from_secs(600))
+        .filter(move |f| f.data.custom_id == ACCEPT_BTN)
+        .build();
+
+    while let Some(interaction) = collector.next().await {
+        if interaction.user.id == challenger_id {
+            ephemeral_interaction_response(&ctx, interaction, "You cannot fight yourself.")
+                .await
+                .ok()?;
+            continue;
+        }
+
+        return Some(interaction);
+    }
+
+    None
 }
 
 async fn get_user_weapon_choice(
