e22338e11f1c625cbb67c6ad56520825cc1c85ec
Riccardo Fano <rcr.fano@gmail.com>
2024-11-08 21:11:58 +0100

    Create and implement CoreReplyContext

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,4 +1,6 @@
-use anyhow::Context as AnyhowContext;
+mod real;
+
+use ::serenity::all::MessageId;
 use chrono::{DateTime, Utc};
 use poise::serenity_prelude::{self as serenity};
 use poise::CreateReply;
@@ -7,17 +9,21 @@ use sqlx::Sqlite;
 
 use crate::Result;
 
+#[derive(Debug, Clone)]
 pub struct MockUser;
+#[derive(Debug, Clone)]
 pub struct MockMember;
-pub struct MockReplyHandle;
+#[derive(Debug, Clone)]
 pub struct MockCollector;
+#[derive(Debug, Clone)]
+pub struct MockMessage;
 
 #[cfg_attr(test, mockall::automock(
     type Data=crate::Data;
     type Database=Pool<Sqlite>;
     type User=MockUser;
     type Member=MockMember;
-    type ReplyHandle=MockReplyHandle;
+    type ReplyHandle=MockCoreReplyHandle;
     type Collector=MockCollector;
 ))]
 pub trait CoreContext {
@@ -25,7 +31,7 @@ pub trait CoreContext {
     type User;
     type Member;
 
-    type ReplyHandle;
+    type ReplyHandle: CoreReplyHandle<Context = Self>;
     type Collector;
 
     fn data(&self) -> Self::Data;
@@ -47,84 +53,12 @@ pub trait CoreContext {
     async fn timeout(&self, member: Option<Self::Member>, until: DateTime<Utc>);
 }
 
-/**
- * Real context implementation
- */
-impl<'a> CoreContext for poise::Context<'a, crate::Data, crate::Error> {
-    type Data = &'a crate::Data;
-    type User = serenity::User;
-    type Member = serenity::Member;
+#[cfg_attr(test, mockall::automock(type Context = MockCoreContext; type Message = MockMessage;))]
+pub trait CoreReplyHandle {
+    type Context: CoreContext;
+    type Message: Clone;
 
-    type ReplyHandle = poise::ReplyHandle<'a>;
-    type Collector = serenity::all::ComponentInteractionCollector;
-
-    fn data(&self) -> Self::Data {
-        poise::Context::data(*self)
-    }
-    async fn acquire_db_connection(&self) -> Result<PoolConnection<Sqlite>> {
-        self.data()
-            .database
-            .acquire()
-            .await
-            .context("Failed to acquire database connection")
-    }
-
-    fn author(&self) -> &Self::User {
-        poise::Context::author(*self)
-    }
-    async fn author_member(&self) -> Option<Self::Member> {
-        let m = poise::Context::author_member(*self).await;
-        m.map(|m| m.into_owned())
-    }
-
-    fn user_id(&self, user: &Self::User) -> serenity::UserId {
-        user.id
-    }
-    async fn user_nickname(&self, user: &Self::User) -> Option<String> {
-        let guild_id = self.guild_id()?;
-        user.nick_in(&self, guild_id).await
-    }
-    async fn user_name(&self, user: &Self::User) -> String {
-        self.user_nickname(user)
-            .await
-            .unwrap_or_else(|| user.name.clone())
-    }
-    async fn user_colour(&self, user: &Self::User) -> Option<serenity::Colour> {
-        self.guild_id()?
-            .member(&self, user.id)
-            .await
-            .ok()?
-            .colour(self)
-    }
-    fn user_avatar_url(&self, user: &Self::User) -> String {
-        user.avatar_url()
-            .unwrap_or_else(|| user.default_avatar_url())
-    }
-
-    async fn reply(&self, builder: CreateReply) -> Result<()> {
-        self.send(builder).await?;
-        Ok(())
-    }
-    async fn bail(&self, content: String) -> Result<()> {
-        let reply = CreateReply::default().content(content).ephemeral(true);
-        self.send(reply).await?;
-        Ok(())
-    }
-    async fn reply_with_handle(&self, builder: CreateReply) -> Result<Self::ReplyHandle> {
-        self.send(builder).await.context("Failed to send reply")
-    }
-
-    fn create_collector(&self) -> Self::Collector {
-        Self::Collector::new(self)
-    }
-    async fn timeout(&self, member: Option<Self::Member>, until: DateTime<Utc>) {
-        if let Some(mut member) = member {
-            if let Err(e) = member
-                .disable_communication_until_datetime(self, until.into())
-                .await
-            {
-                eprintln!("Failed to timeout {:?}, reason: {e:?}", member);
-            }
-        }
-    }
+    async fn into_message(self) -> Result<Self::Message>;
+    async fn message_id(&self) -> Result<MessageId>;
+    async fn edit(&self, ctx: Self::Context, builder: CreateReply) -> Result<()>;
 }
